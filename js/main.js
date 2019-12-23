2c0251bc8f91e35a498a15e0bc2da5a9c3849167
Colum Cross <cxc9401@rit.edu>
2019-12-22 20:46:46 -0500

    Current Portfolio Progress

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/**
+ * Main JS for website.
+ */
+
+/**
+ * @name expandDong
+ * @description Expands an element, so you can read more.
+ * @param elem The element to be expanded.
+ */
+function expandDong(elem) {
+    
+}
+
+/**
+ * @name collapseElement
+ * @description Collapses the element.
+ * @param elem The element to be collapsed.
+ */
+function collapseElement(elem) {
+    
+}
